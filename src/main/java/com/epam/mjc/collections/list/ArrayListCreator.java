43ece3e57d10d78880453ea457f7eb06bf41f294
Apoffis <abc22_08@mail.ru>
2022-10-28 13:14:19 +0400

    collections task1 commit

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListCreator {
+
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> resList = new ArrayList<>();
+        for (int i = 2; i < sourceList.size(); i += 3) {
+            resList.add(sourceList.get(i));
+            resList.add(sourceList.get(i));
+        }
+        return resList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,32 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
+
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
+
     @Override
     public int compare(String a, String b) {
+        int aToInt = Integer.parseInt(a);
+        int bToInt = Integer.parseInt(b);
+
+        if (formula(aToInt) > formula(bToInt)) return 1;
+        else if (formula(aToInt) < formula(bToInt)) return -1;
+        else {
+            return Integer.compare(aToInt, bToInt);
+        }
+    }
+
+    double formula(int num) {
+        return 5 * Math.pow(num, 2) + 3;
     }
 }
